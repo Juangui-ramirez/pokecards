8ee8c53bc7b1882575b1d3197fbfc2d3d1e6d5a1
Juangui <juan-ramirez04@hotmail.com>
2023-07-05 21:59:58 -0500

    Create filter by pokemon (not complete)

--- root/scripts/buycards.js
+++ root/scripts/buycards.js
@@ -2,61 +2,74 @@ let url = "https://pokeapi.co/api/v2";
 let offset = 0;
 let limit = 20;
 let countCards = 0;
+// let type = "";
 
-//Function that creates pokemon cards with their info
+const renderCards = (pokemons) => {
+  pokemons.forEach(async (pokemon) => {
+    const urlPokemon = await fetch(pokemon.url);
+    const dataPokemon = await urlPokemon.json();
+
+    //Create card Pokemon
+    let pokeCard = document.createElement("div");
+    pokeCard.className = "pokeCard";
+    pokeCard.innerHTML = `
+          <div class = "headerCard">
+              <p>${dataPokemon.name}</p>
+              <i class = "fa-sharp fa-regular fa-heart"></i>
+          </div>
+          <img class = "imgPoke" src = "${dataPokemon.sprites.other["home"].front_default}">
+          <div class = "footerCard">
+              <p>Exp ${dataPokemon.base_experience}</p>
+              <a href = "./pokemonDetails.html?pokemon=${pokemon.id}">
+              <button class = "btnCard">Buy</button>
+              </a>
+          </div>`;
+
+    //Select div container and push cards
+    const container = document.querySelector(".grid-container");
+    container.appendChild(pokeCard);
+
+    //Count cards
+    countCards++;
+    const totalCards = document.querySelector(".cardsCount");
+    totalCards.textContent = `${countCards} Cards`;
+
+    //Change img for your shiny version
+    const imgPoke = pokeCard.querySelector(".imgPoke");
+    imgPoke.addEventListener("click", async () => {
+      if (imgPoke.src === dataPokemon.sprites.other["home"].front_shiny) {
+        imgPoke.classList.toggle("flipped");
+        setTimeout(() => {
+          imgPoke.src = dataPokemon.sprites.other["home"].front_default;
+        }, 500); // Change img after 500 ms (0.5 seg)
+      } else {
+        setTimeout(() => {
+          imgPoke.src = dataPokemon.sprites.other["home"].front_shiny;
+        }, 500);
+        imgPoke.classList.toggle("flipped");
+      }
+    });
+  });
+}
+
+let pokemons = [];
+
 const getPokemon = async () => {
   try {
     // Get data for API and results its an URL for every pokemon
-    const res = await fetch(`${url}/pokemon?offset=${offset}&limit=${limit}`);
-    const data = await res.json();
-
-    //Get all data for every pokemon
-    data.results.forEach(async (pokemon) => {
-      const urlPokemon = await fetch(pokemon.url);
+    let pokeList;
+    const allPokemon = await fetch(`${url}/pokemon?limit=100000&offset=0`);
+    const PokeAlldata = await allPokemon.json();
+    pokemons = await PokeAlldata.results.map(async (p) => {
+      const urlPokemon = await fetch(p.url);
       const dataPokemon = await urlPokemon.json();
+      return dataPokemon.results
+    })
 
-      //Create card Pokemon
-      let pokeCard = document.createElement("div");
-      pokeCard.className = "pokeCard";
-      pokeCard.innerHTML = `
-            <div class = "headerCard">
-                <p>${dataPokemon.name}</p>
-                <i class = "fa-sharp fa-regular fa-heart"></i>
-            </div>
-            <img class = "imgPoke" src = "${dataPokemon.sprites.other["home"].front_default}">
-            <div class = "footerCard">
-                <p>Exp ${dataPokemon.base_experience}</p>
-                <a href = "./pokemonDetails.html?pokemon=${pokemon.id}">
-                <button class = "btnCard">Buy</button>
-                </a>
-            </div>`;
-
-      //Select div container and push cards
-      const container = document.querySelector(".grid-container");
-      container.appendChild(pokeCard);
-
-      //Count cards
-      countCards++;
-      const totalCards = document.querySelector(".cardsCount");
-      totalCards.textContent = `${countCards} Cards`;
-
-      //Change img for your shiny version
-      const imgPoke = pokeCard.querySelector(".imgPoke");
-      imgPoke.addEventListener("click", async () => {
-        if (imgPoke.src === dataPokemon.sprites.other["home"].front_shiny) {
-          imgPoke.classList.toggle("flipped");
-          setTimeout(() => {
-            imgPoke.src = dataPokemon.sprites.other["home"].front_default;
-          }, 500); // Change img after 500 ms (0.5 seg)
-        } else {
-          setTimeout(() => {
-            imgPoke.src = dataPokemon.sprites.other["home"].front_shiny;
-          }, 500);
-          imgPoke.classList.toggle("flipped");
-        }
-      });
-    });
-
+    console.log(pokemons);
+    // renderCards(PokeAlldata.results.slice(0 , 20));
+    
+    
     //Replace offset for limit
     offset += limit;
   } catch (error) {
@@ -66,6 +79,32 @@ const getPokemon = async () => {
 
 getPokemon();
 
+
+
 //Get more cards
 const btnMore = document.querySelector(".btnMore");
 btnMore.addEventListener("click", getPokemon);
+
+const typeList = document.querySelectorAll(".navType");
+
+typeList.forEach((typeText) => {
+  typeText.addEventListener("click", (event) => {
+    event.preventDefault();
+    const type = typeText.textContent.toLowerCase();
+
+  });
+});
+
+//Filter Pokemon Cards
+
+const filterByType = async (type) => {
+  const res = await fetch(`${url}/type`);
+  const types = await res.json();
+
+  types.results.forEach(async (types) => {
+    const urlType = await fetch(types.url);
+    const typesPokemon = await urlType.json();
+  });
+};
+
+filterByType();
