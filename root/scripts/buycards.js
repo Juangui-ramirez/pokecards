6d8ed44d10a5851ad83181137f748de928ac12a8
Juangui <juan-ramirez04@hotmail.com>
2023-07-06 19:50:53 -0500

    Fix some functions

--- root/scripts/buycards.js
+++ root/scripts/buycards.js
@@ -1,125 +1,135 @@
 let url = "https://pokeapi.co/api/v2";
 let offset = 0;
 let limit = 20;
-let countCards = 0;
 let pokemons = [];
-// let type = "";
+let currentType = "all";
+
 const container = document.querySelector(".grid-container");
 
+//Function that creates cards and render in hmtml
 const renderCards = (pokemons) => {
-  pokemons.forEach(async (pokemon) => {
-    // const urlPokemon = await fetch(pokemon.url);
-    // const dataPokemon = await urlPokemon.json();
+  container.innerHTML = ""; // Clean container before rendering new cards
 
-    //Create card Pokemon
+  //Create card Pokemon
+  pokemons.forEach((pokemon) => {
     let pokeCard = document.createElement("div");
     pokeCard.className = "pokeCard";
     pokeCard.innerHTML = `
-          <div class = "headerCard">
-              <p>${pokemon.name}</p>
-              <i class = "fa-sharp fa-regular fa-heart"></i>
-          </div>
-          <img class = "imgPoke" src = "${pokemon.sprites.front_default}">
-          <div class = "footerCard">
-              <p>Exp ${pokemon.base_experience}</p>
-              <a href = "./pokemonDetails.html?pokemon=${pokemon.id}">
-              <button class = "btnCard">Buy</button>
-              </a>
-          </div>`;
+      <div class="headerCard">
+        <p>${pokemon.name}</p>
+        <i class="fa-sharp fa-regular fa-heart"></i>
+      </div>
+      <img class="imgPoke" src="${pokemon.sprites.other["home"].front_default}">
+      <div class="footerCard">
+        <p>Exp ${pokemon.base_experience}</p>
+        <a href="./pokemonDetails.html?pokemon=${pokemon.id}">
+          <button class="btnCard">Buy</button>
+        </a>
+      </div>`;
 
     //Select div container and push cards
     container.appendChild(pokeCard);
 
     //Count cards
-    countCards++;
     const totalCards = document.querySelector(".cardsCount");
-    totalCards.textContent = `${countCards} Cards`;
+    totalCards.textContent = `${pokemons.length} Cards`;
 
     //Change img for your shiny version
     const imgPoke = pokeCard.querySelector(".imgPoke");
     imgPoke.addEventListener("click", async () => {
-      if (imgPoke.src === pokemon.sprites.front_shiny) {
+      if (imgPoke.src === pokemon.sprites.other["home"].front_shiny) {
         imgPoke.classList.toggle("flipped");
         setTimeout(() => {
-          imgPoke.src = pokemon.sprites.front_default;
-        }, 500); // Change img after 500 ms (0.5 seg)
+          imgPoke.src = pokemon.sprites.other["home"].front_default;
+        }, 500);
       } else {
         setTimeout(() => {
-          imgPoke.src = pokemon.sprites.front_shiny;
+          imgPoke.src = pokemon.sprites.other["home"].front_shiny;
         }, 500);
         imgPoke.classList.toggle("flipped");
       }
     });
   });
-}
+};
 
+// Get data for API and results its an URL for every pokemon
 const getPokemon = async () => {
   try {
-    // Get data for API and results its an URL for every pokemon
-    pokemons = localStorage.getItem('pokemons'); // Retrieve cached pokemons from local storage
-    if (!pokemons) {
-      const allPokemon = await fetch(`${url}/pokemon?limit=100000&offset=0`);
-      const allPokeData = await allPokemon.json();
-      const parsedPokemonData = await Promise.all(allPokeData.results.map(async (p) => {
-        const urlPokemon = await fetch(p.url);
-        return  urlPokemon.json();
-        // return dataPokemon.results
-      }))
+    const res = await fetch(`${url}/pokemon?limit=100000&offset=0`);
+    const data = await res.json();
 
-      pokemons = parsedPokemonData.map(({ name, sprites, types, base_experience }) => (
-        { name, sprites: sprites.other.home , types, base_experience }
-      ))
+    pokemons = data.results.map((pokemon) => ({
+      url: pokemon.url,
+      name: pokemon.name,
+      sprites: {},
+      types: [],
+      base_experience: 0,
+    }));
 
-      console.log(pokemons)
-      localStorage.setItem('pokemons', JSON.stringify(pokemons)); // Cache pokemons in local storage
-    } else {
-      //Get pkes from cache
-      pokemons = JSON.parse(pokemons);
-    }
+    await Promise.all(
+      pokemons.map(async (pokemon) => {
+        const res = await fetch(pokemon.url);
+        const data = await res.json();
+
+        pokemon.sprites = data.sprites;
+        pokemon.types = data.types;
+        pokemon.base_experience = data.base_experience;
+      })
+    );
+
     renderCards(pokemons.slice(offset, limit));
   } catch (error) {
-    console.log(error)
     alert("Url not found");
   }
 };
 
-//Filter Pokemon Cards
+//Get more cards
+const loadMoreCards = () => {
+  offset += limit;
 
-const filterByType = async (type) => {
-  container.innerHTML = ''
-  offset = 0
-  limit = 20
-  countCards = 0
-  if (type !== 'all') {
-    const filteredPokemon = pokemons.filter(pokemon => {
-      return pokemon.types.some(pokemonType => {
+  if (currentType === "all") {
+    renderCards(pokemons.slice(0, offset + limit));
+  } else {
+    const filteredPokemon = pokemons.filter((pokemon) => {
+      return pokemon.types.some((pokemonType) => {
+        return pokemonType.type.name === currentType;
+      });
+    });
+
+    renderCards(filteredPokemon.slice(0, offset + limit));
+  }
+};
+
+//Filter Pokemon Cards
+const filterByType = (type) => {
+  offset = 0;
+  currentType = type;
+
+  if (type === "all") {
+    renderCards(pokemons.slice(0, limit));
+  } else {
+    const filteredPokemon = pokemons.filter((pokemon) => {
+      return pokemon.types.some((pokemonType) => {
         return pokemonType.type.name === type;
       });
     });
-  
-    renderCards(filteredPokemon.slice(offset, limit));
-  } else {
-    renderCards(pokemons.slice(offset, limit))
+
+    renderCards(filteredPokemon.slice(0, limit));
   }
 };
 
 getPokemon();
 
-//Get more cards
 const btnMore = document.querySelector(".btnMore");
-btnMore.addEventListener("click", () => {
-  offset += limit
-  limit += limit
+btnMore.addEventListener("click", loadMoreCards);
 
-  renderCards(pokemons.slice(offset, limit))
-});
-
+//Get text from html nav and execute filter
 const typeList = document.querySelectorAll(".navType");
 
 typeList.forEach((typeText) => {
   typeText.addEventListener("click", (event) => {
     event.preventDefault();
     const type = typeText.textContent.toLowerCase();
-    filterByType(type)
+    filterByType(type);
   });
 });
